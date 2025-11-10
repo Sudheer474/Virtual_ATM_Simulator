4b5299f8075a4c56502a4eeddf3dd19f8472292e
Sudheer, Mishra <mishrasudheer472@gmail.com>
2025-11-10 23:50:59 +0530

    DESC: initial Spring Boot project setup for Virtual ATM Simulator

---
+++ src/main/java/com/virtualatm/atmsimulator/VirtualAtmSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.virtualatm.atmsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualAtmSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualAtmSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/virtualatm/atmsimulator/VirtualAtmSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtualatm.atmsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualAtmSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
